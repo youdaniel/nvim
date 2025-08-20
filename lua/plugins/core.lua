1beca88571d7c621598707a4547b09441dc8cd00
Daniel You <danielyou416@gmail.com>
2025-08-20 16:07:05 -0400

    bufferline fix

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -28,4 +28,15 @@ return {
       },
     },
   },
+
+  -- https://github.com/LazyVim/LazyVim/pull/6354
+  {
+    "akinsho/bufferline.nvim",
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
+  },
 }
